184206120a1643b05ff94af547a4b00acad95efe
alvinrendi <alvinrendi72@gmail.com>
2024-10-04 08:02:20 +0700

    Alpin Ganteng Banget

--- lib/screens/dboard/dashboard_page.dart
+++ lib/screens/dboard/dashboard_page.dart
@@ -12,7 +12,7 @@ class Dashboard extends StatelessWidget {
         title: Text('DashBoard'),
       ),
       body: const Center(
-        child: Text('Dashboard',
+        child: Text('Alpin Anjeng',
         style: TextStyle(fontSize: 24),
         ),
       )
