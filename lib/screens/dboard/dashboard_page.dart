e147f2801579f01db39eb458c9c612dc7adf0ee2
Muhammad Anindya Prakusya <muhammadanindyaprakusya@gmail.com>
2024-10-06 20:48:19 +0700

    Fufufafa

--- lib/screens/dboard/dashboard_page.dart
+++ lib/screens/dboard/dashboard_page.dart
@@ -1,57 +1,70 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import '../../style/app_properties.dart';
-import '../dashboard/dashboard_page.dart';
+import '../../style/custom_background.dart';
+import '../dashboard/tablet/dashboard_date_page_tablet.dart';
 
-class Dashboard extends StatelessWidget {
+class Dashboard extends StatefulWidget {
   const Dashboard({super.key});
 
   @override
+  _DashboardState createState() => _DashboardState();
+}
+
+class _DashboardState extends State<Dashboard> {
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: Stack(
-          children: [
-            Container(
-              color: Colors.white,
-            ),
-            Align(
-              alignment: Alignment.centerRight,
-              child: Container(
-                width: MediaQuery.of(context).size.width * 0.335,
-                color: const Color(0xFFFECD7E),
-              ),
-            ),
-            const Positioned(
-              left: 20,
-              top: 10,
-              child: Text(
-                'Dashboard',
-                style: TextStyle(
-                  fontSize: 28,
-                  fontWeight: FontWeight.w500,
-                  color: Colors.black,
+    return OrientationBuilder(
+      builder: (context, orientation) {
+        return LayoutBuilder(
+          builder: (context, constraints) {
+            return CustomPaint(
+              painter: MainBackground(),
+              child: Scaffold(
+                backgroundColor: Colors.transparent,
+                appBar: AppBar(
+                  scrolledUnderElevation: 0,
+                  systemOverlayStyle: const SystemUiOverlayStyle(
+                    statusBarColor: Colors.transparent,
+                    statusBarIconBrightness: Brightness.dark,
+                    statusBarBrightness: Brightness.light,
+                  ),
+                  backgroundColor: Colors.transparent,
+                  actions: [
+                    IconButton(
+                      icon: Image.asset(
+                        'assets/icons/calendar.png',
+                        height: 30,
+                        width: 30,
+                      ),
+                      onPressed: () {
+                        showModalBottomSheet(
+                          context: context,
+                          isScrollControlled: true,
+                          isDismissible: false,
+                          builder: (context) => const DashboardDatePageTablet(),
+                        ).then((value) {
+                          setState(() {
+                          });
+                        });
+                      },
+                    )
+                  ],
+                  title: const Text(
+                    'Dashboard',
+                    style: TextStyle(
+                        color: darkGrey,
+                        fontWeight: FontWeight.w500,
+                        fontSize: 28),
+                  ),
                 ),
-              ),
-            ),
-          ],
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            Navigator.of(context).push(
-              MaterialPageRoute(
-                builder: (_) => const DashboardPage(),
               ),
             );
           },
-          backgroundColor: yellow,
-          child: const Icon(
-            Icons.print,
-            color: Colors.black,
-          ),
-        ),
-      ),
+        );
+      },
     );
   }
 }
