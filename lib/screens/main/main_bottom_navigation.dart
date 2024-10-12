d0c4551f48877dc3c7b60c73f8a27aa15aaf5b88
Muhammad Anindya Prakusya <muhammadanindyaprakusya@gmail.com>
2024-10-12 11:18:46 +0700

    Fufufafa

--- lib/screens/main/main_bottom_navigation.dart
+++ lib/screens/main/main_bottom_navigation.dart
@@ -4,6 +4,7 @@ import 'package:dio/dio.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:mozaic_app/api/api_endpoint.dart';
+import 'package:mozaic_app/screens/auth/login_page1.dart';
 import 'package:mozaic_app/screens/main/main_page.dart';
 import 'package:mozaic_app/screens/profile/profile_page.dart';
 import 'package:mozaic_app/widget/custom_snackbar.dart';
@@ -73,7 +74,7 @@ class _MainBottomNavigationState extends State<MainBottomNavigation> with Ticker
   }
 
   List<Widget> listPage = [
-    const MainPage(),
+    const Login(),
     const ExpenditurePage(),
     const DashboardPage(),
     const ProfilePage(),
